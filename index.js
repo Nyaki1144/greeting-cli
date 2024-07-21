fac049a50c9345c9f68b0203e328c584aea338f1
nyaki <narek1144@gmail.com>
2024-07-21 14:46:33 +0400

    feat: add color in color

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { getArguments, getDefaultParametrs, createString } from "./src/tool.js";
 import { getFlags } from "./src/flag.js";
 
+const yellowColor = "\x1b[33m%s\x1b[0m";
+
 function init() {
   const arg = getArguments();
   const flag = getFlags(arg);
-
   const [defGreeting, defName, defDataText] = getDefaultParametrs(flag);
-
   return createString(flag, defGreeting, defName, defDataText);
 }
 
-console.log(init());
+console.log(yellowColor, init());
--- src/flag.js
+++ src/flag.js
@@ -25,8 +25,12 @@ function setFlags(arg, flags) {
 
 function checkingFlags(arg, flags) {
   const reg = /^-|^--/gm;
+  const redColor = "\x1b[31m%s\x1b[0m";
   arg.forEach((el) => {
-    if (reg.test(el) && !(el in flags)) console.log(`${el} is wrong flag, check --help or -h for more information`);
+    if (reg.test(el) && !(el in flags)) {
+      console.error(redColor, `${el} is wrong flag, check --help or -h for more information`);
+      process.exit(1);
+    }
   });
 }
 
